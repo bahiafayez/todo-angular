818e38c6c2eadf8d961484b4c00e8f12c089f226
Hack Reactor Students <accounts@hackreactor.com>
2015-06-13 18:07:59 -0700

    Adding ui-router functionality

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 angular.module('todoApp', ['ui.router'])
-.controller('todoController', function($scope){
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'todo.html',
+    controller: 'todoController'
+  })
+  .state('home.archive', {
+    templateUrl: 'archive.html',
+    controller: 'archiveController'
+  });
+})
+.controller('todoController', function($scope, $state){
   $scope.todos = JSON.parse(window.localStorage.getItem('todos')) || [];
-
+  $state.go('home.archive');
   $scope.addTodo = function(){
-    $scope.todos.push({text: $scope.newEntry, priority: 1});
+    $scope.todos.push({
+      text: $scope.newEntry,
+      priority: 1,
+      completed: false
+    });
     window.localStorage.setItem('todos', angular.toJson($scope.todos));
     $scope.newEntry = "";
   };
 
   $scope.removeTodo = function(todo){
-    $scope.todos.splice($scope.todos.indexOf(todo), 1);
+    todo.completed = true;
+    // $scope.todos.splice($scope.todos.indexOf(todo), 1);
     window.localStorage.setItem('todos', angular.toJson($scope.todos));
   }
 
@@ -22,7 +40,7 @@ angular.module('todoApp', ['ui.router'])
     todo.priority--;
     window.localStorage.setItem('todos', angular.toJson($scope.todos));
   }
-
+})
+.controller('archiveController', function($scope) {
+  console.log($scope.$parent.todos);
 });
-
-
